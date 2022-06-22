302bdd6cbda0b0c6abf29859027babd9b7c3c2b9
Carlos Montoya <camontoya30@gmail.com>
2022-06-22 15:57:35 -0500

    Insert data product js

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 let app = new Vue({
     el: "#app",
     data: {
-        
+        dataProducts: [
+            {
+                tv: [
+                    { id: 1, name: 'LG 3000', price: 1600000, img: 'assets/televisor1.jpg' },
+                    { id: 2, name: 'SAMSUNG', price: 1900000, img: 'assets/televisor2.jpg' }
+                ],
+                fryer: [
+                    { id: 1, name: 'IMUSA 4.2', price: 450000, img: 'assets/freidora1.jpg' },
+                    { id: 2, name: 'HOLSTEIN 3.0', price: 300000, img: 'assets/freidora2.jpg' }
+                ],
+                wash: [
+                    { id: 1, name: 'SAMSUNG 2400', price: 1100000, img: 'assets/lavadora1.jpg' },
+                    { id: 2, name: 'LG 1200', price: 1000000, img: 'assets/lavadora2.jpg' }
+                ],
+                refrigerator: [
+                    { id: 1, name: 'MABE 12', price: 1250000, img: 'assets/nevera1.jpg' },
+                    { id: 2, name: 'WHIRLPOOL', price: 1100000, img: 'assets/nevera2.jpg' }
+                ]
+
+            }
+        ]
     }
 
 })
